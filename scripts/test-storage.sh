8b0079147cd3e9f8465741c7a86d49cf7ee090eb
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-02-14 16:50:43 -0500

    Update scripts/test-storage.sh

--- scripts/test-storage.sh
+++ scripts/test-storage.sh
@@ -7,13 +7,17 @@ pbench-clear-tools
 pbench-clear-results
 
 pbench-register-tool-set --label=FIO
-#readonly REMOTE_HOST=ip-172-31-1-36.us-west-2.compute.internal
-#pbench-register-tool-set --label=FIO --remote="${REMOTE_HOST}"
-oc get node  --no-headers | grep -v master | awk '{print $1}' | while read i; do pbench-register-tool-set --label=FIO --remote="${i}"; done
+
+readonly NODES=($(oc get node  --no-headers | grep -v master | awk '{print $1}'))
+
+for node in "${NODES[@]}"
+do
+  pbench-register-tool-set --label=FIO --remote="${node}"
+done
 
 echo "002 $(date)"
 
-readonly CLIENT_HOST=172.21.0.5
+readonly CLIENT_HOST=172.21.0.19
 
 pbench-fio --test-types=read,write,rw --clients="${CLIENT_HOST}" --config=SEQ_IO --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file=config/sequential_io.job --pre-iteration-script=/root/svt/storage/scripts/drop-cache.sh
 

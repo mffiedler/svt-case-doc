e834b263184da9ee57c0bb6dd63c25a1a5dcd455
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-02-16 15:42:23 -0500

    Update scripts/test-storage.sh

--- scripts/test-storage.sh
+++ scripts/test-storage.sh
@@ -17,15 +17,17 @@ done
 
 echo "002 $(date)"
 
-readonly CLIENT_HOST=$(oc get pod --all-namespaces -o wide --no-headers | grep fio | awk '{print $7}')
+readonly CLIENT_HOSTS_COMMA=$(oc get pod --all-namespaces -o wide --no-headers | grep Running | grep fio | awk '{print $7}' | awk 'BEGIN { ORS = " " } { print }' |  tr " " ,)
 
-echo "CLIENT_HOST ${CLIENT_HOST}"
+readonly CLIENT_HOSTS="${CLIENT_HOSTS_COMMA::-1}"
 
-pbench-fio --test-types=read,write,rw --clients="${CLIENT_HOST}" --config=SEQ_IO --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file=config/sequential_io.job --pre-iteration-script=/root/svt/storage/scripts/drop-cache.sh
+echo "CLIENT_HOST ${CLIENT_HOSTS}"
+
+pbench-fio --test-types=read,write,rw --clients="${CLIENT_HOSTS}" --config=SEQ_IO --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file=config/sequential_io.job --pre-iteration-script=/root/svt/storage/scripts/drop-cache.sh
 
 echo "003 $(date)"
 
-pbench-fio --test-types=randread,randwrite,randrw --clients="${CLIENT_HOST}" --config=RAND_IO --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file=config/random_io.job --pre-iteration-script=/root/svt/storage/scripts/drop-cache.sh
+pbench-fio --test-types=randread,randwrite,randrw --clients="${CLIENT_HOSTS}" --config=RAND_IO --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file=config/random_io.job --pre-iteration-script=/root/svt/storage/scripts/drop-cache.sh
 
 echo "pbench-copy-results: $(date)"
 

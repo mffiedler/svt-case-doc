f49ca374c96007bf8c63250e872163b6d10b44a9
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-03-23 16:24:03 -0400

    Update cases/mongodb_storage.md

---
+++ scripts/test-mongo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+readonly ITERATION=10
+
+readonly MONGO_IP=$(oc get svc -n ttt mongodb -o json | jq '.spec.clusterIP' --raw-output)
+
+for i in $(seq 1 ${ITERATION});
+do
+  echo ${i}
+  oc exec $(oc get pod -n ttt | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -P workloads/workloadt/workloadt -p mongodb.url=mongodb://redhat:redhat@${MONGO_IP}:27017/testdb
+  oc exec $(oc get pod -n ttt | grep m-cli | awk '{print $1}') -- mongo -u redhat -p redhat ${MONGO_IP}:27017/testdb --eval "db.usertable.remove({})"
+done

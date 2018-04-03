57dd81aa9906da67e0200589fabd2229019cd55f
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-04-03 10:23:42 -0400

    Update test_jenkins.sh

--- scripts/test_jenkins.sh
+++ scripts/test_jenkins.sh
@@ -2,6 +2,17 @@
 
 python -V
 
+### Generate logs
 curl -L -O https://raw.githubusercontent.com/openshift/svt/master/openshift_scalability/content/logtest/root/ocp_logtest.py
 
-python ocp_logtest.py --line-length 128 --word-length 7 --rate 3000 --time 600 --fixed-line
+python ocp_logtest.py --line-length 128 --word-length 7 --rate 3000 --time 3 --fixed-line
+
+### Generate artifacts
+mkdir -p build
+rm -rf build/*
+dd if=/dev/zero of=build/output.dat1  bs=1M  count=128
+dd if=/dev/zero of=build/output.dat2  bs=1M  count=128
+
+tar -cvzf build.tar.gz build
+
+

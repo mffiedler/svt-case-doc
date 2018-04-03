083ff464d7d7940ce717626d19dba9e0a6b21cb2
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-04-03 09:28:33 -0400

    Update test_jenkins.sh

--- scripts/test_jenkins.sh
+++ scripts/test_jenkins.sh
@@ -4,4 +4,4 @@ python -V
 
 curl -L -O https://raw.githubusercontent.com/openshift/svt/master/openshift_scalability/content/logtest/root/ocp_logtest.py
 
-python ocp_logtest.py --line-length 128 --word-length 7 --rate 3000 --time 3 --fixed-line
+python ocp_logtest.py --line-length 128 --word-length 7 --rate 3000 --time 600 --fixed-line

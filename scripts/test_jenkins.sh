85e314b55cde4f96dc2a5a0c60ffa38748388e9b
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-04-03 10:41:25 -0400

    Update test_jenkins.sh

--- scripts/test_jenkins.sh
+++ scripts/test_jenkins.sh
@@ -14,9 +14,7 @@ rm -rf build/*
 dd if=/dev/zero of=build/output.dat1  bs=1M  count=128
 dd if=/dev/zero of=build/output.dat2  bs=1M  count=128
 
-echo "BUILD_NUMBER:${BUILD_NUMBER}."
-
-filename="build_${BUILD_NUMBER}.tar.gz"
+filename="build_${BUILD_TAG}_${BUILD_ID}.tar.gz"
 
 echo "filename:${filename}."
 

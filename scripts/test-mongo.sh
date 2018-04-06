5bc662999a909dfd235e7f4ce149d1385ffb4a30
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-04-06 13:26:28 -0400

    Update test-mongo.sh

--- scripts/test-mongo.sh
+++ scripts/test-mongo.sh
@@ -9,7 +9,7 @@ readonly MONGO_IP=$(oc get svc -n ttt mongodb -o json | jq '.spec.clusterIP' --r
 for i in $(seq 1 ${ITERATION});
 do
   echo ${i}
-  oc exec $(oc get pod -n ttt | grep m-cli | awk '{print $1}') -- mongo -u redhat -p redhat ${MONGO_IP}:27017/testdb --eval "db.usertable.remove({})"
+  oc exec $(oc get pod -n ttt | grep mongodb | awk '{print $1}') -- mongo -u redhat -p redhat ${MONGO_IP}:27017/testdb --eval "db.usertable.remove({})"
   oc exec $(oc get pod -n ttt | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads 60 -P workloads/workload_template -p mongodb.url=mongodb://redhat:redhat@${MONGO_IP}:27017/testdb
 done
 

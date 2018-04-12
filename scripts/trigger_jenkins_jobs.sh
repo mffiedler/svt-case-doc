a41ed0ae57d8e9dae496315820e46d778f1945fe
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-04-12 17:09:09 -0400

    update scripts/trigger_jenkins_jobs.sh

--- scripts/trigger_jenkins_jobs.sh
+++ scripts/trigger_jenkins_jobs.sh
@@ -11,6 +11,8 @@ oc exec -n ttt "${JJB_POD}" -- curl -L -o /data/download_job_files.sh https://ra
 
 oc exec -n ttt "${JJB_POD}" -- bash /data/download_job_files.sh
 
+sleep 10
+
 oc exec -n ttt "${JJB_POD}" -- jenkins-jobs --flush-cache  update --delete-old /data
 
 function trigger()
@@ -22,7 +24,52 @@ function trigger()
   curl -k --user admin:password -X POST "https://${url}/job/${job_name}/build" --data-urlencode json='{"parameter": []}'
 }
 
-
 for i in $(seq 0 29); do trigger "${JENKINS_URL}" "test-${i}_job"; done
 
-###TODO: check the result
+function check_build() {
+  local interval
+  interval=$1
+  local timeout
+  timeout=$2
+  local start_time
+  start_time=$(date +%s)
+  local result
+  local all_success
+  local j
+  j=0
+  while (( ($(date +%s) - ${start_time}) < ${timeout} ));
+  do
+    all_success=1
+    for i in $(seq ${j} 29)
+      do
+        j=${i}
+        result=$(curl -s -k --user admin:password https://${JENKINS_URL}/job/test-${i}_job/job/ttt/1/api/json | jq '.result' --raw-output)
+        if [[ "${result}" != "SUCCESS" ]]; then
+          echo "job ${i}: ${result}"
+          all_success=0
+          break
+        fi
+    done
+    if (( ${all_success} == 1 ));
+    then
+      return $(($(date +%s) - ${start_time}))
+    fi
+    sleep ${interval}
+  done
+  return -1
+}
+
+readonly TIMEOUT=600
+check_build 10 ${TIMEOUT}
+MY_TIME=$?
+
+if (( ${MY_TIME} == -1 ));
+then
+  echo "not finished in ${TIMEOUT} seconds"
+  exit 1
+else
+  echo "All builds succeeded in ${MY_TIME} seconds"
+  exit 0
+fi
+
+

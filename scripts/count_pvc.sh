90bc39c179517b2e07fd58b90fcf0a6f1991da0c
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-07-11 09:16:32 -0400

    Update count_pvc.sh

--- scripts/count_pvc.sh
+++ scripts/count_pvc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ### curl -LO https://raw.githubusercontent.com/hongkailiu/svt-case-doc/master/scripts/count_pvc.sh
+### chmod +x count_pvc.sh
 ### * * * * * /root/count_pvc.sh >> /tmp/pvc.log
 readonly PVC_NUMBER=$(oc get pvc --all-namespaces --no-headers | grep Bound | wc -l)
 readonly NOW=$(date '+%Y-%m-%d %H:%M:%S')

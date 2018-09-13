6b414e51e6d8b34448140061c5aca738c1b90550
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-09-12 21:52:49 -0400

    Update scripts/create_jenkins.sh

--- scripts/create_jenkins.sh
+++ scripts/create_jenkins.sh
@@ -35,3 +35,13 @@ do
   oc create -n "${NAMESPACE}" -f ${TMP_FOLDER}/files/oc/dc_jjb.yaml
   sleep 10
 done
+
+
+###
+###
+# grep finish pbench-user-benchmark_jenkins_storage_test_50_2_glusterfs-storage-block_6144Mi_2018.09.12T18.54.16/1/reference-result/result.txt | grep -v "not f" | awk '{print $NF}' | paste -sd+ | bc
+# for i in {1..50}; do echo "index $i"; ll pbench-user-benchmark_jenkins_storage_test_50_2_glusterfs-storage-block_6144Mi_2018.09.12T18.54.16/1/reference-result/jenkins_result*brief* | cut -d "/" -f4 | grep "jenkins_result_run_storage-test-jenkins-${i}_" | wc -l; done
+# for i in {1..100}; do echo $i; oc exec -n storage-test-jenkins-${i} $(oc get pod -n storage-test-jenkins-${i} | grep -v deploy | grep jenkins | awk '{print $1}') -- rm -rfv "/var/lib/jenkins/.m2/repository/"; done
+# oc get pod --all-namespaces | grep jenkins | grep -v Running | awk '{print $1}' | while read i; do echo $i; oc delete rc -n $i jenkins-1; sleep 20; done
+# grep -irn finish pbench-user-benchmark_jenkins_storage_test_50_2_glusterfs-storage-block_6144Mi_2018.09.12T18.54.16/1/reference-result/jenkins_result*brief* | grep -v "not finish" | awk '{print $NF}'  | paste -sd+ | bc
+# for i in {51..200}; do oc delete project "storage-test-jenkins-$i" --wait=false; sleep 30; done

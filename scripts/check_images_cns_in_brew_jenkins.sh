c1feb41f3d52dc99d58331a3bcd7ad417496ddab
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-09-18 16:29:36 -0400

    Create check_images_cns_in_brew_jenkins.sh

---
+++ scripts/check_images_cns_in_brew_jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+function check_brew_latest(){
+  local image
+  image=$1
+  echo "checking image: ${image}"
+  skopeo inspect --tls-verify=false docker://brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/$image | jq .RepoTags | jq -r .[] | grep -v candidate | grep -v HOTFIX | sort -V | grep latest -B1
+}
+
+
+
+check_brew_latest "rhgs3/rhgs-gluster-block-prov-rhel7"
+check_brew_latest "rhgs3/rhgs-volmanager-rhel7"
+check_brew_latest "rhgs3/rhgs-server-rhel7"
+check_brew_latest "rhgs3/rhgs-s3-server-rhel7"

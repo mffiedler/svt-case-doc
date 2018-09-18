23312ba0a1468e174dd3a733682ad8c8adf3a20a
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-09-18 16:34:35 -0400

    Update check_images_cns_in_brew_jenkins.sh

--- scripts/check_images_cns_in_brew_jenkins.sh
+++ scripts/check_images_cns_in_brew_jenkins.sh
@@ -6,7 +6,9 @@ function check_brew_latest(){
   local image
   image=$1
   echo "checking image: ${image}"
-  skopeo inspect --tls-verify=false docker://brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/$image | jq .RepoTags | jq -r .[] | grep -v candidate | grep -v HOTFIX | sort -V | grep latest -B1
+  local tag
+  tag=$(skopeo inspect --tls-verify=false docker://brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/$image | jq .RepoTags | jq -r .[] | grep -v candidate | grep -v HOTFIX | sort -V | grep latest -B1 | head -n 1)
+  echo "brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/$image:$tag"
 }
 
 
